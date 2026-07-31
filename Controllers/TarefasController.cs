6dcba44dd5ac651788d2b4e4e13072413de29b7f
jnior128-tech <jnior128@gmail.com>
2026-07-30 21:06:47 -0300

    chore: restaura senha padrao do banco de dados no appsettings

--- Controllers/TarefasController.cs
+++ Controllers/TarefasController.cs
@@ -1,149 +1,142 @@
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using avaliacao_09_charles_gabriel_karina_lucas.Models;
+using avaliacao_09_charles_gabriel_karina_lucas.Data;
 
-public class TarefasController : Controller
+namespace avaliacao_09_charles_gabriel_karina_lucas.Controllers
 {
-    private readonly avaliacao_09_charles_gabriel_karina_lucasContext _context;
-
-    public TarefasController(avaliacao_09_charles_gabriel_karina_lucasContext context)
+    public class TarefasController : Controller
     {
-        _context = context;
-    }
+        private readonly AppDbContext _context;
 
-    
-    public async Task<IActionResult> Index()    
-    {
-        return View(await _context.Tarefa.ToListAsync());
-    }
-
-  
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
+        public TarefasController(AppDbContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var tarefa = await _context.Tarefa
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (tarefa == null)
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            return View(await _context.Tarefas.ToListAsync());
         }
 
-        return View(tarefa);
-    }
-
-    // GET: TAREFAS/Create
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: TAREFAS/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,Data,Concluida")] Tarefa tarefa)
-    {
-        if (ModelState.IsValid)
+        public async Task<IActionResult> Details(int? id)
         {
-            _context.Add(tarefa);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,Concluida,DataCriacao")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tarefa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarefa);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Descricao,Concluida,DataCriacao")] Tarefa tarefa)
+        {
+            if (id != tarefa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tarefa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TarefaExists(tarefa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarefa);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa != null)
+            {
+                _context.Tarefas.Remove(tarefa);
+            }
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        return View(tarefa);
-    }
 
-    // GET: TAREFAS/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
+        private bool TarefaExists(int id)
         {
-            return NotFound();
+            return _context.Tarefas.Any(e => e.Id == id);
         }
-
-        var tarefa = await _context.Tarefa.FindAsync(id);
-        if (tarefa == null)
-        {
-            return NotFound();
-        }
-        return View(tarefa);
-    }
-
-    // POST: TAREFAS/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int? id, [Bind("Id,Titulo,Descricao,Data,Concluida")] Tarefa tarefa)
-    {
-        if (id != tarefa.Id)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(tarefa);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TarefaExists(tarefa.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(tarefa);
-    }
-
-    // GET: TAREFAS/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var tarefa = await _context.Tarefa
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (tarefa == null)
-        {
-            return NotFound();
-        }
-
-        return View(tarefa);
-    }
-
-    // POST: TAREFAS/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int? id)
-{
-        var tarefa = await _context.Tarefa.FindAsync(id);
-        if (tarefa != null)
-    {
-            _context.Tarefa.Remove(tarefa);
-        }
-
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool TarefaExists(int? id)
-    {
-        return _context.Tarefa.Any(e => e.Id == id);
     }
 }
